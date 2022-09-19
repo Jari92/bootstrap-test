2f371fcab1acf3d1ffc788b34e6f7298dea83715
Jari <janine.riss@gmail.com>
2022-09-19 11:27:29 +0200

    first content changes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.101.0">
-    <title>Blog Template · Bootstrap v5.2</title>
+    <title>Jari's Blog · Bootstrap v5.2</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/blog/">
 
@@ -86,7 +86,7 @@
         <a class="link-secondary" href="#">Subscribe</a>
       </div>
       <div class="col-4 text-center">
-        <a class="blog-header-logo text-dark" href="#">Large</a>
+        <a class="blog-header-logo text-dark" href="#">Jari's Blog</a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
         <a class="link-secondary" href="#" aria-label="Search">
@@ -118,8 +118,8 @@
 <main class="container">
   <div class="p-4 p-md-5 mb-4 rounded text-bg-dark">
     <div class="col-md-6 px-0">
-      <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
+      <h1 class="display-4 fst-italic">My IT Knowledge - Gone in the wind?</h1>
+      <p class="lead my-3">How much of my knowledge is still there or ever really was, after the birth of my son and taking a long parental leave. As a lateral entrant to get a job in IT was challenging enough. How will my baby brain cope with the storm of information for reentering the job? </p>
       <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
     </div>
   </div>
