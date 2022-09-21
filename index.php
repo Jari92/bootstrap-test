5a2681d4077a15113b4fa26801feb4c4394de2fc
Jari <janine.riss@gmail.com>
2022-09-21 11:10:18 +0200

    image tests

--- index.php
+++ index.php
@@ -126,8 +126,7 @@
           <a href="#" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+          <img src="../img/Jari_Dark_portal_with_fog_on_mossy_hill_c1a2951d-bfa5-4a27-b8f9-7a63babaef76.png" class="img-fluid" alt="Responsive image">
         </div>
       </div>
     </div>
