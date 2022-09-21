e58ae4f6327c5b06bf65ce16f7e754723ca1e452
Jari <janine.riss@gmail.com>
2022-09-21 11:28:39 +0200

    spacing tests

--- index.php
+++ index.php
@@ -120,13 +120,15 @@
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
           <strong class="d-inline-block mb-2 text-primary">World</strong>
-          <h3 class="mb-0">Coming back into the IT world</h3>
+          <h3 class="mb-4">Coming back into the IT world</h3>
           <div class="mb-1 text-muted">Sep 19</div>
           <p class="card-text mb-auto">As a lateral entrant to get a job in IT was challenging enough. How will my baby brain cope with the storm of information for reentering the job?</p>
           <a href="#" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
-          <img src="../img/Jari_Dark_portal_with_fog_on_mossy_hill_c1a2951d-bfa5-4a27-b8f9-7a63babaef76.png" class="img-fluid" alt="Responsive image">
+          <!--  img src="../img/Jari_Dark_portal_with_fog_on_mossy_hill_c1a2951d-bfa5-4a27-b8f9-7a63babaef76.png" class="img-fluid" alt="Responsive image"-->
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          
         </div>
       </div>
     </div>
