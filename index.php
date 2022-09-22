a76d8fa144ba90e74913e42f5c79fa1ab11ca331
Jari <janine.riss@gmail.com>
2022-09-22 22:21:29 +0200

    bg-dark l.83

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
   <body>
     
 <div class="container">
-  <header class="blog-header lh-1 py-3">
+  <header class="blog-header bg-dark lh-1 py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
       <div class="col-4 pt-1">
         <a class="link-secondary" href="#">Subscribe</a>
